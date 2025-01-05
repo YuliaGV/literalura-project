70fe6fcd89341f8969f4e8dbcdb571fb89ba557a
YuliaGV <ygaviria7@gmail.com>
2025-01-05 14:28:58 -0500

    Initial commit

---
+++ src/main/java/com/alura/literalura_project/LiteraluraProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literalura_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraProjectApplication.class, args);
+	}
+
+}
